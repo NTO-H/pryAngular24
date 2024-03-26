2ab11c6bc44d1caa0407ffe1a922e682d78e3c4c
anthony= <134122438+Nikrovikey@users.noreply.github.com>
2024-03-26 06:34:01 -0600

    subiendo =>iot

--- src/app/models/dispositivos.ts
+++ src/app/models/dispositivos.ts
@@ -1,13 +1,15 @@
 export class Dispositivo {
     _id?: number;
     Temperatura: string;
+    humedad: string;
     Movimiento: string;
     EstadoLed: number;
 
 
 
-    constructor(Temperatura: string, EstadoLed:number,Movimiento: string) {
+    constructor(Temperatura: string, EstadoLed: number, humedad: string, Movimiento: string) {
         this.Temperatura = Temperatura;
+        this.humedad = humedad;
         this.Movimiento = Movimiento;
         this.EstadoLed = EstadoLed;
     }
