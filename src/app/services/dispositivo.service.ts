2e39312d3855e2800fba36d7a6fcfab3885ad564
anthony= <134122438+Nikrovikey@users.noreply.github.com>
2024-03-28 20:23:47 -0600

    subiendo diseño

--- src/app/services/dispositivo.service.ts
+++ src/app/services/dispositivo.service.ts
@@ -1,7 +1,8 @@
+import { Dispositivo } from '../models/dispositivos';
 import { HttpClient } from '@angular/common/http';
+// Es el módulo de Angular utilizado para realizar solicitudes HTTP.
 import { Injectable } from '@angular/core';
-import { Observable, forkJoin } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 
 @Injectable({
     providedIn: 'root',
@@ -12,29 +13,41 @@ export class DispositivoService {
 
     constructor(private http: HttpClient) { }
 
-    getAllDispositivosData(): Observable<any> {
-        const estadoLed$ = this.http.get<any>(this.url + 'obtenerEstadoLed');
-        const tempHum$ = this.http.get<any>(this.url + 'obtenerEstadoTemperaturaHumedad');
-        const estadoValancin$ = this.http.get<any>(this.url + 'obtenerEstadoValancin');
-        const estadoMusica$ = this.http.get<any>(this.url + 'obtenerEstadoMusica');
-        const estadoCarrucel$ = this.http.get<any>(this.url + 'obtenerEstadoCarrucel');
-
-        return forkJoin({
-            estadoLed: estadoLed$,
-            tempHum: tempHum$,
-            estadoValancin: estadoValancin$,
-            estadoMusica: estadoMusica$,
-            estadoCarrucel: estadoCarrucel$
-        }).pipe(
-            map(data => {
-                return {
-                    estadoLed: data.estadoLed,
-                    tempHum: data.tempHum,
-                    estadoValancin: data.estadoValancin,
-                    estadoMusica: data.estadoMusica,
-                    estadoCarrucel: data.estadoCarrucel
-                };
-            })
-        );
+    getEstadoLed(): Observable<any> {
+        return this.http.get<any>(this.url +'obtenerEstadoLed');
     }
+    getTempHum(): Observable<any> {
+        return this.http.get<any>(this.url +'obtenerEstadoTemperaturaHumedad');
+    }
+    getEstadoValancin(): Observable<any> {
+        return this.http.get<any>(this.url +'obtenerEstadoValancin');
+    }
+    getEstadoMusica(): Observable<any> {
+        return this.http.get<any>(this.url +'obtenerEstadoMusica');
+    }
+
+    getEstadoCarrucel(): Observable<any> {
+        return this.http.get<any>(this.url + 'obtenerEstadoCarrucel');
+    }
+
+    obtenerProducto(id: string): Observable<any> {
+        return this.http.get(this.url + id);
+    }
+
+    editarDispositivoLed(estado: number): Observable<any> {
+        return this.http.put(this.url + "cambiarEstadoLed", {"led": estado });
+    }
+
+    editarDispositivoValancin(estadoValancin: number): Observable<any> {
+        return this.http.put(this.url + "cambiarEstadoValancin", { "valancin": estadoValancin });
+    }
+
+    editarDispositivoCarrucel(estadoCarrucel: number): Observable<any> {
+        return this.http.put(this.url + "cambiarEstadoCarrucel", { "carrucel": estadoCarrucel });
+    }
+
+    editarEstadoMusica(estadoMusica: number): Observable<any> {
+        return this.http.put(this.url + "cambiarEstadoMusica", { "musica": estadoMusica });
+    }
+
 }
