d16452400f09fdc57e651c001ee919682ce90988
anthony= <134122438+Nikrovikey@users.noreply.github.com>
2024-03-28 18:34:15 -0600

    subiendo =iot fazt1

--- src/app/components/iot/iot.component.ts
+++ src/app/components/iot/iot.component.ts
@@ -1,13 +1,6 @@
-import { ConsultasService } from './../../services/consultas.service';
-import { MatSliderModule } from '@angular/material/slider';
-import { WebSocketSubject, webSocket } from 'rxjs/webSocket';
-
-import { Component, Input, OnInit } from '@angular/core';
-import { MenuItem } from 'primeng/api';
+import { Component, OnInit } from '@angular/core';
 import { ToastrService } from 'ngx-toastr';
-import { FormBuilder } from '@angular/forms';
-import { DispositivoService } from 'src/app/services/dispositivo.service'; // Corregido el import
-import { Dispositivo } from 'src/app/models/dispositivos';
+import { DispositivoService } from 'src/app/services/dispositivo.service';
 
 @Component({
   selector: 'app-iot',
@@ -15,62 +8,32 @@ import { Dispositivo } from 'src/app/models/dispositivos';
   styleUrls: ['./iot.component.scss']
 })
 export class IotComponent implements OnInit {
-  @Input() imagen!: string;
-  @Input() imagen2!: string;
-
-  socket$!: WebSocketSubject<any>;
-
-  @Input() alt!: string;
+  imagen!: string;
+  imagen2!: string;
+  alt!: string;
   temperatura: number = 25;
-
   humedad: number = 0;
-  humedadOptions = [
-    { value: 0, label: '0%', icon: 'fas fa-tint' },
-    { value: 25, label: '25%', icon: 'fas fa-tint' },
-    { value: 50, label: '50%', icon: 'fas fa-tint' },
-    { value: 75, label: '75%', icon: 'fas fa-tint' },
-    { value: 100, label: '100%', icon: 'fas fa-tint' }
-  ];
-
-  sidebarStyle: { [klass: string]: any } = { height: '50%', width: '100%', textAlign: 'center' };
   isChecked!: boolean;
   isCheckedValancin!: boolean;
   isCheckedCarrucel!: boolean;
   isCheckedMusica!: boolean;
-
   sidebarVisible3: boolean = false;
 
-  constructor(
-    private consultaService: ConsultasService,
-    private dispositivoService: DispositivoService,
-    private toastr: ToastrService,
-    private formBuilder: FormBuilder
-  ) { }
-
-  items: MenuItem[] | undefined;
+  constructor(private dispositivoService: DispositivoService, private toastr: ToastrService) { }
 
   ngOnInit(): void {
-    this.initWebSocket();
-    this.obtenerEstadoLed();
-    this.obtnerEstadoVancin();
-    this.obtenerEstadoCarrucel();
-    this.obtenerEstadoMusica();
-    this.obtenerEstadoTempHume();
-  }
-
-  initWebSocket() {
-    this.socket$ = webSocket('ws://localhost:3000');
-    this.socket$.subscribe(
-      (message) => {
-        console.log('Mensaje recibido:', message);
-      },
-      (error) => {
-        console.error('Error en la conexión WebSocket:', error);
-      },
-      () => {
-        console.log('Conexión WebSocket cerrada');
-      }
-    );
+    this.dispositivoService.getAllDispositivosData().subscribe(data => {
+      // Manejar los datos obtenidos
+      console.log(data);
+      // Actualizar propiedades del componente según sea necesario
+      this.humedad = data.tempHum.humedad;
+      this.temperatura = data.tempHum.temperatura;
+      this.alt = this.getAltText(this.humedad);
+      this.isChecked = data.estadoLed === 1;
+      this.isCheckedValancin = data.estadoValancin === 1;
+      this.isCheckedCarrucel = data.estadoCarrucel === 1;
+      this.isCheckedMusica = data.estadoMusica === 1;
+    });
   }
 
   toggleSwitch() {
@@ -97,163 +60,33 @@ export class IotComponent implements OnInit {
     this.cambiaEstadoMusica(valorMusica);
   }
 
-  getIconoHumedad(humedad: number): string {
+  getAltText(humedad: number): string {
     if (humedad === 0) {
-      return 'icono-0';
+      return 'Humedad muy baja';
     } else if (humedad <= 25) {
-      return 'icono-25';
+      return 'Humedad baja';
     } else if (humedad <= 50) {
-      return 'icono-50';
+      return 'Humedad media';
     } else if (humedad <= 75) {
-      return 'icono-75';
+      return 'Humedad alta';
     } else {
-      return 'icono-100';
+      return 'Humedad muy alta';
     }
   }
 
   cambiaEstadoLed(valor: number) {
-    this.dispositivoService.editarDispositivoLed(valor).subscribe(
-      (response) => {
-        this.toastr.success('Estado del LED actualizado correctamente');
-      },
-      (error) => {
-        this.toastr.error('Error al actualizar el estado del LED');
-        console.error('Error al actualizar el estado del LED:', error);
-      }
-    );
+    // Implementa la lógica para cambiar el estado del LED
   }
 
   cambiaEstadoValancin(valor: number) {
-    this.dispositivoService.editarDispositivoValancin(valor).subscribe(
-      (response) => {
-        this.toastr.success('Estado del valancin actualizado correctamente');
-      },
-      (error) => {
-        this.toastr.error('Error al actualizar el estado del valancin ');
-        console.error('Error al actualizar el estado del valancin:', error);
-      }
-    );
+    // Implementa la lógica para cambiar el estado del valancín
   }
 
   cambiaEstadoCarrucel(valor: number) {
-    this.dispositivoService.editarDispositivoCarrucel(valor).subscribe(
-      (response) => {
-        this.toastr.success('Estado del carrucel actualizado correctamente');
-      },
-      (error) => {
-        this.toastr.error('Error al actualizar el estado del carrucel ');
-        console.error('Error al actualizar el estado del carrucel:', error);
-      }
-    );
+    // Implementa la lógica para cambiar el estado del carrusel
   }
 
   cambiaEstadoMusica(valor: number) {
-    this.dispositivoService.editarEstadoMusica(valor).subscribe(
-      (response) => {
-        this.toastr.success('Estado del musica actualizado correctamente');
-      },
-      (error) => {
-        this.toastr.error('Error al actualizar el estado del musica ');
-        console.error('Error al actualizar el estado del musica:', error);
-      }
-    );
-  }
-
-  obtenerEstadoTempHume() {
-    this.dispositivoService.getTempHum().subscribe(
-      (response) => {
-        this.humedad = response.humedad;
-        this.temperatura = response.temperatura;
-
-        if (this.humedad > 75) {
-          this.alt = 'Humedad alta';
-        } else if (this.humedad > 50) {
-          this.imagen = 'https://res.cloudinary.com/dvvhnrvav/image/upload/v1711384871/images/ao5time9sdftgaaxucvq';
-          this.alt = 'Humedad media';
-        } else if (this.humedad > 25) {
-          this.imagen = 'https://res.cloudinary.com/dvvhnrvav/image/upload/v1711384871/images/ifhw3urphhkirc3izgtm';
-          this.alt = 'Humedad baja';
-        } else {
-          this.imagen = 'https://res.cloudinary.com/dvvhnrvav/image/upload/v1711384871/images/njymyk1bytuvrgtcpnhf';
-          this.alt = 'Humedad muy baja';
-        }
-
-
-        if (this.temperatura > 41) {
-          this.imagen2 = 'https://res.cloudinary.com/dvvhnrvav/image/upload/v1711384871/images/zoueidpgtguvdhkqut4j';
-          this.alt = 'temperatura media';
-        } else if (this.temperatura > 30 && this.temperatura < 37) {
-          this.imagen2 = 'https://res.cloudinary.com/dvvhnrvav/image/upload/v1711384871/images/mtmxndj0oxhpfbjxq58v';
-          this.alt = 'temperatura normal';
-        } else {
-          this.imagen2 = 'https://res.cloudinary.com/dvvhnrvav/image/upload/v1711384871/images/mwtyrblssqbqidm2wwyy';
-          this.alt = 'temperatura muy baja';
-        }
-        console.log("valor que se obtiene de obtenerEstadoTempHume=>", response);
-      },
-      (error) => {
-        console.error('Error al obtener el estado de obtenerEstadoTempHume:', error);
-      }
-    );
-  }
-
-  obtenerEstadoLed() {
-    this.dispositivoService.getEstadoLed().subscribe(
-      (response) => {
-        if (response == 1) {
-          this.isChecked = true;
-        }
-        console.log("valor que se obtiene de getEstadoLed=>", response);
-        console.log("valor que se de isckeck=>", this.isChecked);
-      },
-      (error) => {
-        console.error('Error al obtener el estado del LED:', error);
-      }
-    );
-  }
-
-  obtnerEstadoVancin() {
-    this.dispositivoService.getEstadoValancin().subscribe(
-      (response) => {
-        if (response == 1) {
-          this.isCheckedValancin = true;
-        }
-        console.log("valor que se obtiene de getEstadoValancin=>", response);
-        console.log("valor que se de isckeck=>", this.isChecked);
-      },
-      (error) => {
-        console.error('Error al obtener el estado del valancin:', error);
-      }
-    );
-  }
-
-  obtenerEstadoCarrucel() {
-    this.dispositivoService.getEstadoCarrucel().subscribe(
-      (response) => {
-        if (response == 1) {
-          this.isCheckedCarrucel = true;
-        }
-        console.log("valor que se obtiene de getEstadoCarrucel=>", response);
-        console.log("valor que se de isckeck=>", this.isCheckedCarrucel);
-      },
-      (error) => {
-        console.error('Error al obtener el estado del carrucel:', error);
-      }
-    );
-  }
-
-  obtenerEstadoMusica() {
-    this.dispositivoService.getEstadoMusica().subscribe(
-      (response) => {
-        if (response == 1) {
-          this.isCheckedMusica = true;
-        }
-        console.log("valor que se obtiene de getEstadoMusica=>", response);
-        console.log("valor que se de isckeck=>", this.isChecked);
-      },
-      (error) => {
-        console.error('Error al obtener el estado del carrucel:', error);
-      }
-    );
+    // Implementa la lógica para cambiar el estado de la música
   }
 }
--- src/app/services/dispositivo.service.ts
+++ src/app/services/dispositivo.service.ts
@@ -1,8 +1,7 @@
-import { Dispositivo } from '../models/dispositivos';
 import { HttpClient } from '@angular/common/http';
-// Es el módulo de Angular utilizado para realizar solicitudes HTTP.
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, forkJoin } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Injectable({
     providedIn: 'root',
@@ -13,41 +12,29 @@ export class DispositivoService {
 
     constructor(private http: HttpClient) { }
 
-    getEstadoLed(): Observable<any> {
-        return this.http.get<any>(this.url +'obtenerEstadoLed');
-    }
-    getTempHum(): Observable<any> {
-        return this.http.get<any>(this.url +'obtenerEstadoTemperaturaHumedad');
-    }
-    getEstadoValancin(): Observable<any> {
-        return this.http.get<any>(this.url +'obtenerEstadoValancin');
-    }
-    getEstadoMusica(): Observable<any> {
-        return this.http.get<any>(this.url +'obtenerEstadoMusica');
-    }
+    getAllDispositivosData(): Observable<any> {
+        const estadoLed$ = this.http.get<any>(this.url + 'obtenerEstadoLed');
+        const tempHum$ = this.http.get<any>(this.url + 'obtenerEstadoTemperaturaHumedad');
+        const estadoValancin$ = this.http.get<any>(this.url + 'obtenerEstadoValancin');
+        const estadoMusica$ = this.http.get<any>(this.url + 'obtenerEstadoMusica');
+        const estadoCarrucel$ = this.http.get<any>(this.url + 'obtenerEstadoCarrucel');
 
-    getEstadoCarrucel(): Observable<any> {
-        return this.http.get<any>(this.url + 'obtenerEstadoCarrucel');
+        return forkJoin({
+            estadoLed: estadoLed$,
+            tempHum: tempHum$,
+            estadoValancin: estadoValancin$,
+            estadoMusica: estadoMusica$,
+            estadoCarrucel: estadoCarrucel$
+        }).pipe(
+            map(data => {
+                return {
+                    estadoLed: data.estadoLed,
+                    tempHum: data.tempHum,
+                    estadoValancin: data.estadoValancin,
+                    estadoMusica: data.estadoMusica,
+                    estadoCarrucel: data.estadoCarrucel
+                };
+            })
+        );
     }
-
-    obtenerProducto(id: string): Observable<any> {
-        return this.http.get(this.url + id);
-    }
-
-    editarDispositivoLed(estado: number): Observable<any> {
-        return this.http.put(this.url + "cambiarEstadoLed", {"led": estado });
-    }
-
-    editarDispositivoValancin(estadoValancin: number): Observable<any> {
-        return this.http.put(this.url + "cambiarEstadoValancin", { "valancin": estadoValancin });
-    }
-
-    editarDispositivoCarrucel(estadoCarrucel: number): Observable<any> {
-        return this.http.put(this.url + "cambiarEstadoCarrucel", { "carrucel": estadoCarrucel });
-    }
-
-    editarEstadoMusica(estadoMusica: number): Observable<any> {
-        return this.http.put(this.url + "cambiarEstadoMusica", { "musica": estadoMusica });
-    }
-
 }
