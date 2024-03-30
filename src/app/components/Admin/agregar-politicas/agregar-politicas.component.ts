ba6dd809cc8cc2809146a94ec42432eb2e7915d9
anthony= <134122438+Nikrovikey@users.noreply.github.com>
2024-03-29 19:56:04 -0600

    Politicas eliminary actualizar  v10.16.3

--- src/app/components/Admin/agregar-politicas/agregar-politicas.component.ts
+++ src/app/components/Admin/agregar-politicas/agregar-politicas.component.ts
@@ -1,6 +1,6 @@
 import { ActivatedRoute, Router } from '@angular/router';
 import { AdminService } from './../../../services/admin.service';
-import { Component,OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import Swal from 'sweetalert2';
 // import { AdminService } from './../../../services/admin.service';
@@ -10,7 +10,7 @@ import { error } from 'console';
 // ReactiveFormsModule
 @Component({
   selector: 'app-agregar-politicas',
-  
+
   templateUrl: './agregar-politicas.component.html',
   styleUrl: './agregar-politicas.component.scss'
 })
@@ -28,65 +28,65 @@ export class AgregarPoliticasComponent {
   titulo = 'Agregar Politica';
   btnTitle = 'Agregar';
 
-  
-  id : string | null;
-  listPoliticas: Politica[]=[]
-  
-  constructor(private aRouter:ActivatedRoute,private adminService:AdminService,private router:Router,private toastr: ToastrService ,private formBuilder: FormBuilder) {
-    
+
+  id: string | null;
+  listPoliticas: Politica[] = []
+
+  constructor(private aRouter: ActivatedRoute, private adminService: AdminService, private router: Router, private toastr: ToastrService, private formBuilder: FormBuilder) {
+
 
 
 
 
     this.frmAgregarPoliticas = this.formBuilder.group({
-    titulo: ['', Validators.required],
-    contenido: ['', Validators.required]
-    
+      titulo: ['', Validators.required],
+      contenido: ['', Validators.required]
+
     })
-this.id=this.aRouter.snapshot.paramMap.get( 'id')
-    
+    this.id = this.aRouter.snapshot.paramMap.get('id')
+
 
   }
 
-  
+
 
   ngOnInit(): void {
     this.obtenerPoliticas();
     this.esEditar();
-    
+
   }
 
   obtenerPoliticas() {
     this.adminService.getPoliticas().subscribe(data => {
       this.listPoliticas = data;
       console.log(data)
-    
+
     }, error => {
-    console.log("ocurrio un error al obtener las politicas")
+      console.log("ocurrio un error al obtener las politicas")
     })
 
 
-    
+
 
 
   }
-  
 
-  eliminarPolitica(id:any){
+
+  eliminarPolitica(id: any) {
     console.log("esEliminar=>", id)
-    
 
-    
-    this.adminService.eliminarPolitica(id).subscribe((data)=>{
+
+
+    this.adminService.eliminarPolitica(id).subscribe((data) => {
       this.obtenerPoliticas()
-    this.toastr.success('Politica eliminado con éxito','Politica eliminado')
-    
+      this.toastr.success('Politica eliminado con éxito', 'Politica eliminado')
+
     }, error => {
       this.obtenerPoliticas()
       this.toastr.error('Politica no  elimando', 'Falló al eliminar')
 
     })
-    
+
 
   }
 
@@ -94,36 +94,36 @@ this.id=this.aRouter.snapshot.paramMap.get( 'id')
 
 
   agregarPolitica() {
-   
-   
+
+
     const POLITICA: Politica = {
       titulo: this.frmAgregarPoliticas.get('titulo')?.value,
       contenido: this.frmAgregarPoliticas.get('contenido')?.value,
-     
+
     };
 
-   
+
     if (this.frmAgregarPoliticas.get('politica')?.value === '') {
       Swal.fire('Error', 'Por favor selecciona una pregunta', 'error');
       return; // No permitir enviar el formulario si no se ha seleccionado una pregunta
-    } 
+    }
 
     // const politica=this.frmAgregarPoliticas.get('politicas')?.value
-     console.log("id obtenido=>",this.id)
-      if(this.id!==null){
-        this.adminService.editarPolitica(this.id, POLITICA).subscribe(()=>{
-        
+    console.log("id obtenido=>", this.id)
+    if (this.id !== null) {
+      this.adminService.editarPolitica(this.id, POLITICA).subscribe(() => {
+        this.visible = false;
         this.toastr.success('Politica actualizado correctamente')
 
-          this.obtenerPoliticas()
-        }, (error) => {
+        this.obtenerPoliticas()
+      }, (error) => {
         this.toastr.error('Ocurrio un error al actualizar la politica')
-        }
-          
-        )        
-      } else {
+      }
+
+      )
+    } else {
       this.adminService.registrarPoliticas(POLITICA).subscribe(data => {
-      
+
         console.log('Respuesta 02:', data)
         this.toastr.success('Politica registrado con éxito!', 'Registró éxitoso');
         this.obtenerPoliticas()
@@ -131,23 +131,23 @@ this.id=this.aRouter.snapshot.paramMap.get( 'id')
       }, error => {
         // this.router.navigate(['/agregar-politicas']);
         this.toastr.error('ocurrio un error!', 'Error');
-        
+
       })
-      
+
     }
   }
 
-  
+
   esEditar() {
-  
+
     if (this.id !== null) {
       this.titulo = 'Editar Politica';
       this.btnTitle = 'Actualizar';
       this.adminService.obtenerPolitica(this.id).subscribe((data) => {
         this.frmAgregarPoliticas.setValue(
           {
-            titulo : data.titulo,
-            contenido :data.contenido
+            titulo: data.titulo,
+            contenido: data.contenido
           })
       })
     }
@@ -157,23 +157,18 @@ this.id=this.aRouter.snapshot.paramMap.get( 'id')
   editar(_id: any) {
     // this.position = position;
     this.visible = true;
-    this.id = _id; 
-  console.log("esEditar",_id)
+    this.id = _id;
+    console.log("esEditar", _id)
     if (_id) {
       this.titulo = 'Editar Politica';
       this.btnTitle = 'Actualizar';
       this.adminService.obtenerPolitica(_id).subscribe((data) => {
         this.frmAgregarPoliticas.setValue(
           {
-            titulo : data.titulo,
-            contenido :data.contenido
+            titulo: data.titulo,
+            contenido: data.contenido
           })
       })
     }
   }
-
-
-
-
-
 }
