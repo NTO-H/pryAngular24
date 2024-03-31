c05a758585ceee8b417ec062f47d851bf25e8a1f
anthony= <134122438+Nikrovikey@users.noreply.github.com>
2024-03-31 00:34:25 -0600

    iot nuev modulo 12.1

--- src/app/IOT/iot.module.ts
+++ src/app/IOT/iot.module.ts
@@ -1,11 +1,15 @@
+import { TablaDispositivosComponent } from './../components/IOTDevs/tabla-dispositivos/tabla-dispositivos.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { IotRoutingModule } from './iot-routing.module';
+import { DashboardsComponent } from '../components/IOTDevs/dashboards/dashboards.component';
+import { DevicesComponent } from '../components/IOTDevs/devices/devices.component';
+// import { TablaDispositivosComponent } from '../components/IOTDevs/tabla-dispositivos/tabla-dispositivos.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [DashboardsComponent, TablaDispositivosComponent,DevicesComponent],
   imports: [
     CommonModule,
     IotRoutingModule
--- src/app/components/IOTDevs/devices/devices.component.ts
+++ src/app/components/IOTDevs/devices/devices.component.ts
@@ -2,8 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-devices',
-  standalone: true,
-  imports: [],
   templateUrl: './devices.component.html',
   styleUrl: './devices.component.scss'
 })
