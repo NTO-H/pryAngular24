3b68e18ee40292a3c3a613a3557d549f10dfe21d
anthony= <134122438+Nikrovikey@users.noreply.github.com>
2024-03-31 08:45:37 -0600

    iot home 7

--- src/app/components/IOTDevs/iot-home/iot-home.component.ts
+++ src/app/components/IOTDevs/iot-home/iot-home.component.ts
@@ -9,4 +9,20 @@ import { Component } from '@angular/core';
 })
 export class IotHomeComponent {
 
+  
+
+  sidebarVisible3: boolean = false;
+
+
+  mostrarSidebar() {
+    this.sidebarVisible3 = true;
+  }
+
+
+  copiarClave() {
+    // Obtener el valor del input
+    const claveInput = document.getElementById('keyInput') as HTMLInputElement;
+    claveInput.select();
+    document.execCommand('copy');
+  }
 }
