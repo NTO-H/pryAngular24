c4871a8815581113f9c762461c5a0e96889cd0de
anthony= <134122438+Nikrovikey@users.noreply.github.com>
2024-03-31 11:17:45 -0600

    iot home 8.9.7

--- src/app/components/IOTDevs/iot-home/iot-home.component.ts
+++ src/app/components/IOTDevs/iot-home/iot-home.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { AuthService } from './../../../services/auth.service';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-iot-home',
@@ -14,10 +15,39 @@ export class IotHomeComponent {
   sidebarVisible3: boolean = false;
 
 
+
+  usuarioId!: String;
+
+  constructor(private auth: AuthService) { }
   mostrarSidebar() {
     this.sidebarVisible3 = true;
+
+    this.getToken();
+
+
   }
 
+  // ngOnInit(): void {
+  //   //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+  //   //Add 'implements OnInit' to the class.
+  //   this.getToken();
+  // }
+  
+  getToken() {
+    const userData = localStorage.getItem('currentUser');
+  
+   console.log(userData)
+    // Verificar si hay datos de usuario en el localStorage
+    if (userData) {
+      // Obtener los datos del usuario por correo electrónico
+      this.auth.miPerfilUsuario(userData).subscribe(data => {
+        this.usuarioId = data._id;
+      });
+    }
+  }
+    
+  
+  
 
   copiarClave() {
     // Obtener el valor del input
@@ -25,4 +55,5 @@ export class IotHomeComponent {
     claveInput.select();
     document.execCommand('copy');
   }
+
 }
