7c8f6ec66f3f9f6ff59d6122e2af2f2386ae6a11
anthony= <134122438+Nikrovikey@users.noreply.github.com>
2024-03-31 12:06:42 -0600

    iot home 10.1

--- src/app/components/IOTDevs/iot-home/iot-home.component.ts
+++ src/app/components/IOTDevs/iot-home/iot-home.component.ts
@@ -1,5 +1,6 @@
 import { AuthService } from './../../../services/auth.service';
 import { Component, OnInit } from '@angular/core';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-iot-home',
@@ -16,7 +17,12 @@ export class IotHomeComponent implements OnInit {
 
 
 
-  usuarioId:any;
+
+  usuarioId!: string |null; // Variable para almacenar el ID del usuario
+
+  // constructor(private authService: AuthService) { }
+
+
 
   constructor(private auth: AuthService) { }
   mostrarSidebar() {
@@ -37,36 +43,29 @@ export class IotHomeComponent implements OnInit {
       });
     }
   }
-
   ngOnInit(): void {
-    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
-    //Add 'implements OnInit' to the class.
     const userData = localStorage.getItem('currentUser');
+    if (userData !== null) {
+      console.log(userData);
+      this.auth.miPerfilUsuario(userData).subscribe(usuario => {
+        this.usuarioId = usuario._id;
+    
+      
+       
+      })
+  }else{
+  
+      Swal.fire('Error', 'No se obtubo datos', 'error');
 
-    console.log(userData)
-    // Verificar si hay datos de usuario en el localStorage
-    if (userData) {
-      // Obtener los datos del usuario por correo electrónico
-      this.auth.miPerfilUsuario(userData).subscribe(data => {
-        this.usuarioId = data._id;
-        console.log("id>obtenido", this.usuarioId)
-        console.log("id>", data._id)
-      });
     }
-  }
-  
-  // getToken() {
-    
-  // }
     
   
-  
-
-  copiarClave() {
-    // Obtener el valor del input
-    const claveInput = document.getElementById('keyInput') as HTMLInputElement;
-    claveInput.select();
-    document.execCommand('copy');
   }
-
+    copiarClave() {
+      // Obtener el valor del input
+      const claveInput = document.getElementById('keyInput') as HTMLInputElement;
+      claveInput.select();
+      document.execCommand('copy');
+    }
+  
 }
