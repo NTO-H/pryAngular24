c7bce618763febc5210aa14525f882a52c075de5
anthony= <134122438+Nikrovikey@users.noreply.github.com>
2024-03-31 00:19:08 -0600

    iot nuev modulo 11

--- src/app/IOT/iot-routing.module.ts
+++ src/app/IOT/iot-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from '../components/Admin/dashboard/dashboard.component';
+import { DevicesComponent } from '../components/IOTDevs/devices/devices.component';
+import { TablaDispositivosComponent } from '../components/IOTDevs/tabla-dispositivos/tabla-dispositivos.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'devices', component: DevicesComponent },
+  { path: 'tokens', component: TablaDispositivosComponent },
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
