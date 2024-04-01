6ce487a8936f3675c7ee7f1b196ff94fc3ee1b07
anthony= <134122438+Nikrovikey@users.noreply.github.com>
2024-04-01 15:44:59 -0600

    iot home 24.8

--- src/app/components/IOTDevs/iot-home/iot-home.component.ts
+++ src/app/components/IOTDevs/iot-home/iot-home.component.ts
@@ -13,59 +13,59 @@ export class IotHomeComponent implements OnInit {
 
 
 
-  sidebarVisible3: boolean = false;
+  // sidebarVisible3: boolean = false;
 
 
 
 
-  usuarioId: any; // Variable para almacenar el ID del usuario
+  // usuarioId: any; // Variable para almacenar el ID del usuario
 
   // constructor(private authService: AuthService) { }
 
 
 
   constructor(private auth: AuthService) { }
-  mostrarSidebar() {
-    this.sidebarVisible3 = true;
+  // mostrarSidebar() {
+  //   this.sidebarVisible3 = true;
 
-    // this.getToken();
+  //   // this.getToken();
 
-    const userData = localStorage.getItem('currentUser');
+  //   const userData = localStorage.getItem('currentUser');
 
-    console.log(userData)
-    // Verificar si hay datos de usuario en el localStorage
-    if (userData) {
-      // Obtener los datos del usuario por correo electrónico
-      this.auth.miPerfilUsuario(userData).subscribe(data => {
-        this.usuarioId = data.datos;
-        console.log("id>obtenido", this.usuarioId)
-        console.log("id>", data.datos)
-      });
-    }
-  }
+  //   console.log(userData)
+  //   // Verificar si hay datos de usuario en el localStorage
+  //   if (userData) {
+  //     // Obtener los datos del usuario por correo electrónico
+  //     this.auth.miPerfilUsuario(userData).subscribe(data => {
+  //       this.usuarioId = data.datos;
+  //       console.log("id>obtenido", this.usuarioId)
+  //       console.log("id>", data.datos)
+  //     });
+  //   }
+  // }
   ngOnInit(): void {
-    const userData = localStorage.getItem('currentUser');
-    if (userData !== null) {
-      console.log(userData);
-      this.auth.miPerfilUsuario(userData).subscribe(usuario => {
-        this.usuarioId = usuario.datos;
+    // const userData = localStorage.getItem('currentUser');
+    // if (userData !== null) {
+    //   console.log(userData);
+    //   this.auth.miPerfilUsuario(userData).subscribe(usuario => {
+    //     this.usuarioId = usuario.datos;
 
 
 
-      })
-    } else {
+    //   })
+    // } else {
 
-      Swal.fire('Error', 'No se obtubo datos', 'error');
+    //   Swal.fire('Error', 'No se obtubo datos', 'error');
 
-    }
+    // }
 
 
   }
-  copiarClave() {
-    // Obtener el valor del input
-    const claveInput = document.getElementById('keyInput') as HTMLInputElement;
-    claveInput.select();
-    document.execCommand('copy');
-  }
+  // copiarClave() {
+  //   // Obtener el valor del input
+  //   const claveInput = document.getElementById('keyInput') as HTMLInputElement;
+  //   claveInput.select();
+  //   document.execCommand('copy');
+  // }
 
 }
--- src/app/components/IOTDevs/iotheader/iotheader.component.ts
+++ src/app/components/IOTDevs/iotheader/iotheader.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-iotheader',
@@ -11,16 +13,76 @@ export class IotheaderComponent {
   sidebarVisible3: boolean = false;
 
   
-  mostrarSidebar() {
-    this.sidebarVisible3 = true;
-  }
+  // mostrarSidebar() {
+  //   this.sidebarVisible3 = true;
+  // }
 
   
+  // copiarClave() {
+  //   // Obtener el valor del input
+  //   const claveInput = document.getElementById('keyInput') as HTMLInputElement;
+  //   claveInput.select();
+  //   document.execCommand('copy');
+  // }
+
+
+  // sidebarVisible3: boolean = false;
+
+
+
+
+  usuarioId: any; // Variable para almacenar el ID del usuario
+
+  // constructor(private authService: AuthService) { }
+
+
+
+  constructor(private auth: AuthService) { }
+  mostrarSidebar() {
+    this.sidebarVisible3 = true;
+
+    // this.getToken();
+
+    const userData = localStorage.getItem('currentUser');
+
+    console.log(userData)
+    // Verificar si hay datos de usuario en el localStorage
+    if (userData) {
+      // Obtener los datos del usuario por correo electrónico
+      this.auth.miPerfilUsuario(userData).subscribe(data => {
+        this.usuarioId = data.datos;
+        console.log("id>obtenido", this.usuarioId)
+        console.log("id>", data.datos)
+      });
+    }
+  }
+  ngOnInit(): void {
+    const userData = localStorage.getItem('currentUser');
+    if (userData !== null) {
+      console.log(userData);
+      this.auth.miPerfilUsuario(userData).subscribe(usuario => {
+        this.usuarioId = usuario.datos;
+
+
+
+      })
+    } else {
+
+      Swal.fire('Error', 'No se obtubo datos', 'error');
+
+    }
+
+
+  }
   copiarClave() {
     // Obtener el valor del input
     const claveInput = document.getElementById('keyInput') as HTMLInputElement;
     claveInput.select();
     document.execCommand('copy');
   }
+
+
+  
+
 
 }
