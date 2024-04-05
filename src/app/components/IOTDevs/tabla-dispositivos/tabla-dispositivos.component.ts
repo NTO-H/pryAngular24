78e48b9432260116e00e823558eb53d151bd45bb
anthony= <134122438+Nikrovikey@users.noreply.github.com>
2024-04-05 11:27:19 -0600

    rocessandoo 18

--- src/app/components/IOTDevs/tabla-dispositivos/tabla-dispositivos.component.ts
+++ src/app/components/IOTDevs/tabla-dispositivos/tabla-dispositivos.component.ts
@@ -22,7 +22,8 @@ export class TablaDispositivosComponent implements OnInit {
   deviceCount: number = 0;
   mostrarDispositivos: boolean = false;
 
-  filterDevices = '';
+  // filterDevices = '';
+  filterProducts: string = '';
 
 
   constructor(private toastr: ToastrService, private aRouter: ActivatedRoute, private fb: FormBuilder, private dvs: DispositivoService, private usr: UsuarioService) { }
